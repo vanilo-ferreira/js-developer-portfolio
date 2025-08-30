e7450df02b8f37832c3a3855e7cfd0fba190ccd7
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-08-29 23:07:05 -0300

    feat: Adicionar função uptdateProfessionalExperience

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,13 +22,52 @@ function updateProfileInfo(profileData) {
   email.href = `mailto:${profileData.email}`
 }
 
-function updateSoftSkills(profileData){
-  const softSkills = document.getElementById("profile-skils-softskills")
-  softSkills.innerHTML = profileData.skills.softSkills.map( skill => `<li>${skill}</li>`).join("");
+function updateSoftSkills(profileData) {
+  const softSkills = document.getElementById("profile-skills-softskills")
+  softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join("");
 }
+
+function updateHardSkills(profileData) {
+  const hardSkills = document.getElementById("profile-skills-hardSkills")
+  hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src=${skill.logo} alt=${skill.name} title=${skill.name}</li>`).join("");
+}
+
+function updateLanguages(profileData) {
+  const languages = document.getElementById("languages")
+  languages.innerHTML = profileData.language.map(language => `<li>${language}</li>`).join("");
+}
+
+function updatePortfolio(profileData) {
+  const portfolio = document.getElementById("portfolio")
+  portfolio.innerHTML = profileData.portfolio.map(project => {
+    return`
+    <li>
+        <h3 ${project.github ? 'class="github"' : ""}>${project.name}</h3>
+        <a href=${project.url} target="_blank">${project.url}</a>
+    </li>`
+  }).join("")
+}
+
+function uptdateProfessionalExperience(profileData){
+  const professionalExperience = document.getElementById("profile.professionalExperience")
+  professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+    return `
+    <li>
+                        <h3 class="title">${experience.name}</h3>
+                        <p class="period">${experience.period}</p>
+                        <p>${experience.description}</p>
+                    </li>
+    `
+  })
+}
+
 
 (async () => {
   const profileData = await fetchProfileData();
   updateProfileInfo(profileData);
   updateSoftSkills(profileData);
+  updateHardSkills(profileData);
+  updateLanguages(profileData);
+  updatePortfolio(profileData);
+  uptdateProfessionalExperience(profileData);
 })()
